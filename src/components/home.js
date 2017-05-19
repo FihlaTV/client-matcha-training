71daa5b67454f311bc7ecf900a0dbc74e136360b
abarriel <abarriel@student.42.fr>
2017-05-20 00:14:51 +0200

    login done

--- src/components/home.js
+++ src/components/home.js
@@ -32,6 +32,20 @@ class Home extends Component {
               <div className="field-wrap">
                 <input type="text" required autoComplete="off" placeholder="Repeat Password" />
               </div>
+                <button type="submit" className="button button-block">Get Started</button>
+            </form>
+          </div>
+          <div className="Login">
+            <h1>Log In</h1>
+              <form action="/" method="post">
+            <div className="field-wrap">
+              <input type="text" required autoComplete="off" placeholder="User Name" />
+            </div>
+            <div className="field-wrap">
+              <input type="text" required autoComplete="off" placeholder="Password" />
+            </div>
+            <button type="submit" className="button button-block">Log In</button>
+
             </form>
           </div>
         </div>
